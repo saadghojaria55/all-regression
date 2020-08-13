abb6ed48b4b224558ac8ac6fc53d4806e74bb242
Saad Ghojaria <ghojariasaad@gmail.com>
2020-08-13 17:48:47 +0530

    Update multiple_linear_regression.R

--- multiple_linear_regression.R
+++ multiple_linear_regression.R
@@ -1,7 +1,7 @@
 # Multiple Linear Regression
 
 # Importing the dataset
-dataset = read.csv('50_Startups.csv')
+dataset1 = read.csv('50_Startups.csv')
 
 # Encoding categorical data
 dataset$State = factor(dataset$State,
